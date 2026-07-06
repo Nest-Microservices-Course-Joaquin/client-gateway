323e08eb96ab1664d7c12153e20a7d81e295c73f
Moilab <moilab.web@gmail.com>
2026-07-06 14:33:28 +0200

    feat(): config nats module in orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -9,22 +9,20 @@ import {
   Query,
   Patch,
 } from '@nestjs/common';
-import { ORDERS_SERVICE } from 'src/config/services';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
 import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
+import { NATS_SERVICE } from 'src/config/services';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(
-    @Inject(ORDERS_SERVICE) private readonly ordersClient: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
 
   @Post()
   async create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send({ cmd: 'create_order' }, createOrderDto).pipe(
+    return this.natsClient.send({ cmd: 'create_order' }, createOrderDto).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
@@ -33,7 +31,7 @@ export class OrdersController {
 
   @Get()
   async findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersClient
+    return this.natsClient
       .send({ cmd: 'find_all_orders' }, orderPaginationDto)
       .pipe(
         catchError((error) => {
@@ -44,7 +42,7 @@ export class OrdersController {
 
   @Get(':id')
   async findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.ordersClient.send({ cmd: 'find_one_order' }, { id }).pipe(
+    return this.natsClient.send({ cmd: 'find_one_order' }, { id }).pipe(
       catchError((error) => {
         throw new RpcException(error);
       }),
@@ -56,7 +54,7 @@ export class OrdersController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() changeOrderStatusDto: ChangeOrderStatusDto,
   ) {
-    return this.ordersClient
+    return this.natsClient
       .send({ cmd: 'change_order_status' }, { id, ...changeOrderStatusDto })
       .pipe(
         catchError((error) => {
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ORDERS_SERVICE } from 'src/config/services';
-import { envs } from 'src/config/env.validation';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: ORDERS_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: envs.ORDERS_MS_HOST,
-          port: envs.ORDERS_MS_PORT,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
   controllers: [OrdersController],
   providers: [],
 })
