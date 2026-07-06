4057df814bf0091c28e76e9c48461fc6cdeee579
Moilab <moilab.web@gmail.com>
2026-07-06 14:21:02 +0200

    feat(): nats module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config/services';
-import { envs } from 'src/config/env.validation';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.NATS_SERVERS,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
   controllers: [ProductsController],
   providers: [],
 })
